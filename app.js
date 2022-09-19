59f83ecfd8ac65be2b9a89fd3e0349f32541e8ff
Nishaj <njnishaj0@gmail.com>
2022-09-19 20:47:19 +0530

    added li in js that not return input value

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let li = document.createElement('li')
+
+let list = document.getElementById("ul")
+
+function addList(){
+   let val = document.createTextNode("This is a li");
+   const li = document.createElement("li");
+   li.appendChild(val);
+   list.appendChild(li);
+
+}
