624483d02d073e47b21bb1861b50bb7e1197bb4a
Nishaj <njnishaj0@gmail.com>
2022-09-19 21:15:37 +0530

    input will add in ul

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+let ul = document.getElementById("ul")
 
-let list = document.getElementById("ul")
 
-function addList(){
-   let val = document.createTextNode("This is a li");
-   const li = document.createElement("li");
-   li.appendChild(val);
-   list.appendChild(li);
+function addList() {
+   let input = document.getElementById("input").value
 
+   if (input != null) {
+      let val = document.createTextNode(input);
+      const li = document.createElement("li"); /*making html tag and assigning that to a variable */
+
+      li.appendChild(val); /*giving value to li tag */
+      ul.appendChild(li); /*giving li to ul */
+   } else {
+
+   }
 }
